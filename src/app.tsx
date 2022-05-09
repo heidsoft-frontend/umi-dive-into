2e83b5daa4a930da6e4f9dc3d0f6b6f2bbbfd905
jake.liu <jake.liu@baozun.com>
2022-05-09 20:42:24 +0800

    token 模拟使用

--- src/app.tsx
+++ src/app.tsx
@@ -13,14 +13,34 @@ export async function getInitialState(): Promise<API.IUser> {
 }
 
 // 全局拦截器
+const errorHandler = (error: ResponseError) => {
+  //国际化方案
+  //const {messages} = getIntl(getLocale());
+  const { response } = error;
+  notification.error({
+    description: '你的网络发生异常，无法连接服务器',
+    message: '网络异常',
+  });
+};
+
+const authHeaderIntercceptor = (url: string, options: RequestOptions) => {
+  const token = localStorage.getItem('token');
+  if (token !== null) {
+    console.log(token);
+    const authHeader = { Authorization: `Bearer ${token}` };
+    return {
+      url: `${url}`,
+      options: { ...options, interceptors: true, headers: authHeader },
+    };
+  }
+
+  return {
+    url: `${url}`,
+    options: { ...options, interceptors: true },
+  };
+};
+
 export const request: RequestConfig = {
-  errorHandler: (error: ResponseError) => {
-    //国际化方案
-    //const {messages} = getIntl(getLocale());
-    const { response } = error;
-    notification.error({
-      description: '你的网络发生异常，无法连接服务器',
-      message: '网络异常',
-    });
-  },
+  errorHandler,
+  requestInterceptors: [authHeaderIntercceptor],
 };
