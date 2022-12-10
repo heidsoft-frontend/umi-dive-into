ea97079608eb0135394a1d44a730ba8ce0c8213d
jake.liu <jake.liu@baozun.com>
2022-12-10 11:50:43 +0800

    组建嵌套

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,6 +1,50 @@
 import { PageContainer } from '@ant-design/pro-layout';
 import ProTable, { ProColumns } from '@ant-design/pro-table';
+import React, { Component } from 'react';
 
+class Counter extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  //组建点击
+  handleClick(e: any) {
+    e.preventDefault();
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+  //渲染组建
+  //https://github.com/haizlin/fe-interview/issues/952
+  //https://blog.csdn.net/weixin_43459866/article/details/117279110
+  // render(){
+  //   return(
+  //     <div>
+  //       <p>{this.state.count}</p>
+  //       <a href='#' onClick={this.handleClick}>更新</a>
+  //     </div>
+  //   )
+  // }
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <p>{this.state.count}</p>
+          <a href="#" onClick={this.handleClick}>
+            更新
+          </a>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+//产品组建
 function Products() {
   const columns: ProColumns<API.IProduct>[] = [
     {
@@ -14,11 +58,12 @@ function Products() {
     },
   ];
   return (
-    <>
+    <div>
+      {/* <Counter></Counter> */}
       <PageContainer>
         <ProTable<API.IProduct> columns={columns}></ProTable>
       </PageContainer>
-    </>
+    </div>
   );
 }
 
